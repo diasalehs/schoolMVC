76b9721f6b8b9f7fce968504eaa4a490f3b1837f
Dia Saleh <diasaleh@Dias-MacBook-Pro.local>
2016-04-29 01:27:35 +0300

    add teacher arrival

---
+++ app/Teacherarrival.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Teacherarrivale extends Model
+{
+    protected $table='teacherarrivales';
+    protected $fillable=['teacher_id','dayDate','arrivaltime','arrivalDay'];
+    public function teacher(){
+        return $this->belongsTo('App\Teacher');
+    }
+
+}
