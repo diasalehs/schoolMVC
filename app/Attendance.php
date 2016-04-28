8ce320e1fd63facf67aab0cfc65dfc891dc149ed
Dia Saleh <diasaleh@Dias-MacBook-Pro.local>
2016-04-29 02:13:32 +0300

    add attendance

---
+++ app/Attendance.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Attendance extends Model
+{
+    //
+    protected $table='attendances';
+    protected $fillable=['student_id','absanceDate','absanceDay'];
+
+
+}
