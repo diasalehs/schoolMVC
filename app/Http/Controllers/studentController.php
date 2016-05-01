9cbbd9ec94e937ab4211665750ea6d50e19f993d
Mazen shanti <m.shanti@windowslive.com>
2016-05-02 00:49:46 +0300

    add student controller

---
+++ app/Http/Controllers/studentController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mazen
+ * Date: 5/2/2016
+ * Time: 12:38 AM
+ */
+
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+
+class studentController extends Controller{
+    public function getStudent()
+    {
+        if (!Auth::check() or Auth::user()->type != 'student') {
+            return redirect()->route('loginPage');
+        }
+        return view('studentFirst');
+    }
+
+    public function getMessages()
+    {
+        if (!Auth::check() or Auth::user()->type != 'student') {
+            return redirect()->route('loginPage');
+        }
+        return view('studentMessages');
+    }
+
+    public function getMarks()
+    {
+        if (!Auth::check() or Auth::user()->type != 'student') {
+            return redirect()->route('loginPage');
+        }
+        return view('studentMarks');
+    }
+
+
+}
