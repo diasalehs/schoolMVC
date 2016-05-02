2672c09a83ad27cf00e632ad51a2940a6ab7a4f7
Dia Saleh <diasaleh@Dias-MacBook-Pro.local>
2016-05-02 03:28:54 +0300

    add show to teacher

--- resources/views/teacherFirst.blade.php
+++ resources/views/teacherFirst.blade.php
@@ -8,7 +8,7 @@
         <p>اسم المعلم.</p>
     </div>
 
-        <div class="container-fluid">
+        <div class="container-fluid" style="margin-bottom: 150px">
             <div class="row ">
                 <form  class="form-inline" method="post">
                     <div class="fp">
@@ -17,105 +17,101 @@
                         <div class="form-group">
                             <label for="InputEmail" class="ic">جوال</label>
                             <div class="input-group">
-                                <input type="text" class="form-control" id="InputEmailSecond" name="mobile"  >
+                                <input type="text" class="form-control" id="InputEmailSecond" name="mobile"  value="" disabled >
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="InputEmail" class="ic">الهاتف</label>
                             <div class="input-group">
-                                <input type="text" class="form-control" id="InputEmailSecond" name="phone"  >
+                                <input type="text" class="form-control" id="InputEmailSecond" name="phone" disabled >
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label for="InputEmail" class="ic"> الجنسية</label>
                             <div class="input-group">
-                                <input type="text" class="form-control" id="InputEmailSecond" name="nationality"  >
+                                <input type="text" class="form-control" id="InputEmailSecond" name="nationality"disabled  >
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="InputEmail" class="ic"> مكان الولادة</label>
                             <div class="input-group">
-                                <input type="text" class="form-control" id="InputEmailSecond" name="placeOfBirth"  >
+                                <input type="text" class="form-control" id="InputEmailSecond" name="placeOfBirth" disabled >
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label for="InputEmail" class="ic">الديانة</label>
                             <div class="input-group">
-                                <input type="text" class="form-control" id="InputEmailSecond" name="religon"  >
+                                <input type="text" class="form-control" id="InputEmailSecond" name="religon"  disabled>
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="InputEmail" class="ic"> رقم الهوية</label>
                             <div class="input-group">
-                                <input type="text" class="form-control" id="InputEmailSecond" name="ni"  >
+                                <input type="text" class="form-control" id="InputEmailSecond" name="ni" disabled >
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="InputEmail" class="ic"> نوع الهوية</label>
                             <div class="input-group">
-                                <input type="text" class="form-control" id="InputEmailSecond" name="idType"  >
+                                <input type="text" class="form-control" id="InputEmailSecond" name="idType" disabled >
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="InputEmail" class="ic"> البعد عن مكان الاقامة</label>
                             <div class="input-group">
-                                <input type="text" class="form-control" id="InputEmailSecond" name="distanceFromSchool"  >
+                                <input type="text" class="form-control" id="InputEmailSecond" name="distanceFromSchool" disabled >
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="InputEmail" class="ic"> تاريخ الولادة</label>
                             <div class="input-group" >
-                                <input type="date" class="form-control" id="InputEmailSecond" name="dateOfBirth"  >
+                                <input type="text" class="form-control" id="InputEmailSecond" name="dateOfBirth"  disabled>
                             </div>
                         </div>
                         <h3 class="ft">الوضع الاجتماعي والعائلي</h3>
                         <div class="form-group">
                             <label for="InputEmail"> اسم الزوج/ة</label>
                             <div class="input-group" >
-                                <input type="text" class="form-control" id="InputEmailSecond" name="married"  >
+                                <input type="text" class="form-control" id="InputEmailSecond" name="married" disabled >
                             </div>
                         </div>
 
                         <div class="form-group mc" >
                             <label for="InputEmail" > عدد الاطفال</label>
-                            <div class="input-group" style="width: 80px">
-                                <input type="number" class="form-control" id="InputEmailSecond" name="numberOfChildren"  >
+                            <div class="input-group" >
+                                <input type="text" class="form-control" id="InputEmailSecond" name="numberOfChildren" disabled >
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label for="InputEmail">متزوج</label>
                             <div class="input-group">
-                                <select id="sts" class="form-control" >
-                                    <option value="teacher">لا</option>
-                                    <option value="supervisor">نعم</option>
-                                </select>
+                                <input type="text" class="form-control" id="InputEmailSecond" name="numberOfChildren" disabled >
+
+
                             </div>
                         </div>
                         <div class="form-group mc">
                             <label for="InputEmail">هل الزوج او الزوجة يعمل</label>
                             <div class="input-group">
-                                <select id="sts" class="form-control" name="DoesPartnerWork">
-                                    <option value="teacher">لا</option>
-                                    <option value="supervisor">نعم</option>
+                                <input type="text" class="form-control" id="InputEmailSecond" name="numberOfChildren" disabled >
 
-                                </select>
                             </div>
                         </div>
 
 
                         <div class="form-group">
-                            <label for="InputEmail" style="width: 250px"> عدد الاطفال في نفس المدرسة </label>
-                            <div class="input-group" style="width: 80px">
-                                <input type="number" class="form-control" id="InputEmailSecond" name="childrenInSchool"  >
+                            <label for="InputEmail" > عدد الاطفال في نفس المدرسة </label>
+                            <div class="input-group">
+                                <input type="text" class="form-control" id="InputEmailSecond" name="childrenInSchool" disabled >
                             </div>
                         </div>
                         <div class="form-group">
-                            <label for="InputEmail"  style="width: 250px"> عدد الاطفال في المدارس اخرى</label>
-                            <div class="input-group" style="width: 80px">
-                                <input type="number" class="form-control" id="InputEmailSecond" name="childrenOtherSchools"  >
+                            <label for="InputEmail" > عدد الاطفال في المدارس اخرى</label>
+                            <div class="input-group" >
+                                <input type="text" class="form-control" id="InputEmailSecond" name="childrenOtherSchools" disabled >
                             </div>
                         </div>
 
