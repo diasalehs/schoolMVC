14fbb689ab87ca999257df38e329180ca25a1e25
Dia Saleh <diasaleh@Dias-MacBook-Pro.local>
2016-05-03 13:33:29 +0300

    nothing too important

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -69,6 +69,35 @@
     </div>
 </section>
 
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel" style="width: 100%; height: 300px;">
+    <ol class="carousel-indicators">
+        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner" role="listbox">
+        <div class="carousel-item active">
+            <div class="card">
+                <img src="img/" alt="Guitar">
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img data-src="..." alt="Second slide">
+        </div>
+        <div class="carousel-item">
+            <img data-src="..." alt="Third slide">
+        </div>
+    </div>
+    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+        <span class="icon-prev" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+        <span class="icon-next" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
 <div class="album text-muted">
     <div class="container">
 
